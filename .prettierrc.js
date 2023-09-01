151c1af895e74ac71a053b7f921e62a82657192f
Nazarii Lunych <Nazariy.lunich@gmail.com>
2023-09-01 13:36:56 +0300

    First start

---
+++ .prettierrc.js
@@ -0,0 +1,9 @@
+module.exports = {
+  "trailingComma": "es5",
+  "singleQuote": true,
+  "semi": true,
+  "printWidth": 100,
+  "tabWidth": 2,
+  "bracketSpacing": false,
+  "parentheses": "always",
+};
---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import DynamicForm from './components/DynamicForm/DynamicForm';
+import React from 'react';
+
+function App() {
+  return (
+    <div className="App">
+      <DynamicForm />
+    </div>
+  );
+}
+
+export default App;
